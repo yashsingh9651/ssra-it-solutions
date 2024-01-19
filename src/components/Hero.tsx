eaffaffa2c58476c5f4740a08f10086d98880d31
YASH SINGH <123338008+yashsingh9651@users.noreply.github.com>
2024-01-19 22:37:16 +0530

    Milestone:Gsap Animation Added in Front Page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+"use client";
+import React,{useRef,useLayoutEffect} from "react";
 import { phudu, tektur, ubuntu } from "./fonts";
 import { IoFlashOutline } from "react-icons/io5";
 import { RiLightbulbFlashLine } from "react-icons/ri";
 import { GrShieldSecurity } from "react-icons/gr";
 import { MdWeb } from "react-icons/md";
+import gsap, { Power4 } from "gsap";
 const Hero = () => {
+  const heroCom:any = useRef()
+  useLayoutEffect(()=>{
+    const heroCtx = gsap.context(()=>{
+      const t2=gsap.timeline({delay:2.2,defaults:{ease:Power4.easeInOut}});
+      t2.from(".headLine",{y:-150,opacity:0})
+      .from(".gCard",{x:600,stagger:.3,opacity:0,duration:.7})
+      .from(".card",{x:600,stagger:.3,opacity:0,duration:.7},"-=2")
+    },heroCom)
+    return ()=>heroCtx.revert()
+  },[])
   return (
-    <div className="lg:pt-[120px] h-screen overflow-y-hidden p-2">
+    <div ref={heroCom} className="lg:pt-[120px] h-screen overflow-y-hidden p-2">
       <h1
-        className={`${tektur.className} text-3xl lg:text-5xl md:text-6xl md:mt-24 md:mb-16 lg:mb-0 lg:my-28 mt-16 text-white font-semibold capitalize`}
+        className={`${tektur.className} text-3xl lg:text-5xl md:text-6xl md:mt-24 md:mb-16 lg:my-28 mt-16 text-white font-semibold capitalize headLine`}
       >
         Software Consulting and Development <br className="hidden lg:block" /> for your digital success
       </h1>
+      {/* Responsive Cards for Mobile and Tablet */}
+      <div className="grid xl:hidden lg:grid-cols-4 grid-cols-2 gap-5 max-w-fit mx-auto mt-5 lg:mt-12">
+        <div
+          className={`${phudu.className} flex p-2 items-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg gCard`}
+        >
+          Information Technology Consultancy
+        </div>
+        <div
+          className={`${phudu.className} flex p-2 items-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg gCard`}
+        >
+          Product Design Strategy
+        </div>
+        <div
+          className={`${phudu.className} flex p-2 items-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg gCard`}
+        >
+          Cyber Security & Web Development
+        </div>
+        <div
+          className={`${phudu.className} flex p-2 items-center justify-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg gCard`}
+        >
+          UI/UX Design Ideas
+        </div>
+      </div>
       {/* Cards */}
       <div className="xl:flex hidden justify-end gap-4 items-center">
         <div
@@ -33,29 +68,6 @@ const Hero = () => {
           className={`${ubuntu.className} cardH4 relative overflow-hidden flex justify-center items-center w-[180px] h-[240px] text-white text-7xl bg-blue-600 cursor-pointer rounded-2xl card`}
         >
           <MdWeb />
-        </div>
-      </div>
-      {/* Responsive Cards for Mobile and Tablet */}
-      <div className="grid xl:hidden lg:grid-cols-4 grid-cols-2 gap-5 max-w-fit mx-auto mt-5 lg:mt-12">
-        <div
-          className={`${phudu.className} flex p-2 items-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg`}
-        >
-          Information Technology Consultancy
-        </div>
-        <div
-          className={`${phudu.className} flex p-2 items-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg`}
-        >
-          Product Design Strategy
-        </div>
-        <div
-          className={`${phudu.className} flex p-2 items-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg`}
-        >
-          Cyber Security & Web Development
-        </div>
-        <div
-          className={`${phudu.className} flex p-2 items-center justify-center w-[160px] md:w-[240px] h-[220px] md:h-[320px] text-white text-xl text-center bg-blue-600 cursor-pointer rounded-lg`}
-        >
-          UI/UX Design Ideas
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,89 @@
-"use client"
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Button from './Button'
-import { usePathname } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
+import { usePathname } from "next/navigation";
+import { useLayoutEffect, useRef } from "react";
+import gsap, { Back, Circ, Expo, Power4 } from "gsap";
 
 const Navbar = () => {
+  const navCom: any = useRef();
+  useLayoutEffect(() => {
+    const navCtx = gsap.context(() => {
+      const t1 = gsap.timeline();
+      t1.from(".navbar",{y:-160,rotateX:-70,duration:.8,opacity:.3})
+      .from(".navEle",{x:1500,stagger:.3,ease:Power4.easeInOut},"-=.5")
+    }, navCom);
+    return () => navCtx.revert();
+  }, []);
   const path = usePathname();
   return (
-    <div className="w-full flex justify-center">
-      <div className='fixed top-2 lg:top-10 h-11 md:h-[74px] xl:w-3/4 w-[96vw] mx-auto rounded bg-white z-50 flex justify-between p-1 lg:p-2'>
-        <Image alt='logo' src="/images/logo.png" width={300} height={300} className='w-20 md:w-40'/>
-        <div className='flex items-center md:gap-5 gap-3 text-sm md:text-base mr-2 lg:mr-0'>
-          <Link className={`${path=='/'?'text-[#1f2fd8]': 'hover:scale-110 duration-200'} hidden lg:block`} href='/'>Home</Link>
-          <Link className={`${path=='/about'?'text-[#1f2fd8]': 'hover:scale-110 duration-200'}`} href='/about'>About</Link>
-          <Link className={`${path=='/services'?'text-[#1f2fd8]': 'hover:scale-110 duration-200'}`} href='/services'>Services</Link>
-          <Link className={`${path=='/caseStudy'?'text-[#1f2fd8]': 'hover:scale-110 duration-200'}`} href='/caseStudy'>Case-Study</Link>
-          <Link className='hidden md:block' href='/contact'><Button button='Free Consultation' /></Link>
+    <div ref={navCom} className="w-full flex justify-center">
+      <div className="fixed top-2 lg:top-10 h-11 md:h-[74px] xl:w-3/4 w-[96vw] mx-auto rounded bg-white z-50 flex justify-between p-1 lg:p-2 navbar">
+        <Image
+          alt="logo"
+          src="/images/logo.png"
+          width={300}
+          height={300}
+          className="w-20 md:w-40 navEle"
+        />
+        <div className="flex items-center md:gap-5 gap-3 text-sm md:text-base lg:text-lg mr-2 lg:mr-0">
+          <div className="navEle">
+            <Link
+              className={`${
+                path == "/" ? "text-[#1f2fd8]" : "hover:scale-110 duration-200"
+              } hidden lg:block`}
+              href="/"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="navEle">
+            <Link
+              className={`${
+                path == "/about"
+                  ? "text-[#1f2fd8]"
+                  : "hover:scale-110 duration-200"
+              }`}
+              href="/about"
+            >
+              About
+            </Link>
+          </div>
+          <div className="navEle">
+            <Link
+              className={`${
+                path == "/services"
+                  ? "text-[#1f2fd8]"
+                  : "hover:scale-110 duration-200"
+              }`}
+              href="/services"
+            >
+              Services
+            </Link>
+          </div>
+          <div className="navEle">
+            <Link
+              className={`${
+                path == "/caseStudy"
+                  ? "text-[#1f2fd8]"
+                  : "hover:scale-110 duration-200"
+              }`}
+              href="/caseStudy"
+            >
+              Case-Study
+            </Link>
+          </div>
+          <div className="navEle">
+            <Link className="hidden md:block" href="/contact">
+              <Button button="Free Consultation" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
